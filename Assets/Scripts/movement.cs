dd4164431b18e3a40448023c3cc4e9826e045ad4
Wiktoria Ćmiel <117581998+dead-viktor@users.noreply.github.com>
2024-11-16 23:23:29 +0100

    Initial commit

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class movement : MonoBehaviour
+{
+    [SerializeField] float VectorX;
+    [SerializeField] float VectorY = 0;
+    [SerializeField] float VectorZ;
+    [SerializeField] float speed = 5;
+    float score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PrintText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    void PrintText ()
+    {
+        Debug.Log("Sterowanie: WASD");
+    }
+
+    void PlayerMovement()
+    {
+        VectorX = Input.GetAxis("Horizontal");
+        VectorZ = Input.GetAxis("Vertical");
+        transform.Translate(VectorX* Time.deltaTime* speed, VectorY * Time.deltaTime* speed, VectorZ * Time.deltaTime* speed);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        score++;
+        Debug.Log("Zebrane punkty:" + score);
+    }
+}
